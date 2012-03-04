5bbd624997e5b058a83d79985db3dc74c220271d
Chris Andrews <chris@nodnol.org>
2012-03-04 11:20:13 +0000

    Seed the database with a few feed URLs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+feeds = Feed.create([
+    { :name => 'R4 Comedy', :url => 'http://feeds.bbc.co.uk/iplayer/bbc_radio_four/comedy/list' },
+    { :name => 'R4extra Comedy', :url => 'http://feeds.bbc.co.uk/iplayer/bbc_four_extra/comedy/list' },
+    { :name => 'R4 Factual', :url => 'http://feeds.bbc.co.uk/iplayer/bbc_radio_four/factual/list' }
+])
